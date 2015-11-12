f34de7e7f31eb36336f199bb69039526d654c8ec
Christian Mayer <christian@fox21.at>
2015-11-12 21:36:08 +0100

    Show the full path if only one file is selected.

--- lib/id3dit/version.rb
+++ lib/id3dit/version.rb
@@ -1,7 +1,7 @@
 
 module ID3dit
 	NAME = 'ID3dit'
-	VERSION = '0.1.0-dev'
+	VERSION = '1.0.0-dev'
 	HOMEPAGE = 'https://github.com/TheFox/id3dit'
 	USED_TAGS = [:title, :artist, :composer, :year, :genre, :bpm, :grouping, :comment, :album, :band, :disc, :track]
 end
