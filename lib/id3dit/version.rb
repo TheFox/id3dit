1bdc042d0feaf5b566731a0fc19ecf888f9a8414
Christian Mayer <christian@fox21.at>
2015-11-15 15:48:23 +0100

    Version 1.0.0-rc.2.
    - Show name and version on help.
    - Show name and version on interactive mode.
    - Don't show tags that are marked as 'removed' on edit.

--- lib/id3dit/version.rb
+++ lib/id3dit/version.rb
@@ -1,7 +1,7 @@
 
 module ID3dit
 	NAME = 'ID3dit'
-	VERSION = '1.0.0-rc.1'
+	VERSION = '1.0.0-rc.2'
 	HOMEPAGE = 'https://github.com/TheFox/id3dit'
 	USED_TAGS = [:title, :artist, :composer, :year, :genre, :bpm, :grouping, :comment, :album, :band, :disc, :track]
 end
