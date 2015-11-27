a98f1d85269b9a6c123b091eb0ddacb5fc2ae98f
Christian Mayer <christian@fox21.at>
2015-11-27 17:12:06 +0100

    Use ID3Lib IDs to delete tags. #RN

--- lib/id3dit/version.rb
+++ lib/id3dit/version.rb
@@ -1,7 +1,20 @@
 
 module ID3dit
 	NAME = 'ID3dit'
-	VERSION = '1.1.0-dev3'
+	VERSION = '1.1.0-dev4'
 	HOMEPAGE = 'https://github.com/TheFox/id3dit'
-	USED_TAGS = [:title, :artist, :composer, :year, :genre, :bpm, :grouping, :comment, :album, :band, :disc, :track]
+	USED_TAGS = {
+		:title => {:id => :TIT2},
+		:artist => {:id => :TPE1},
+		:composer => {:id => :TCOM},
+		:year => {:id => :TYER},
+		:genre => {:id => :TCON},
+		:bpm => {:id => :TBPM},
+		:grouping => {:id => :TIT1},
+		:comment => {:id => :COMM},
+		:album => {:id => :TALB},
+		:band => {:id => :TPE2},
+		:disc => {:id => :TPOS},
+		:track => {:id => :TRCK},
+	}
 end
